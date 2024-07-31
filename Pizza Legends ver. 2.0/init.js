51461e4cd17470583c9ab1e496998a8d32eb62d8
Juan Manuel Pachas <juanmapv387@gmail.com>
2024-07-31 15:56:21 -0500

    Add files via upload

---
+++ Pizza Legends ver. 2.0/init.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    const overworld = new Overworld ({
+        element: document.querySelector(".game-container")
+
+    });
+    overworld.init();
+});
