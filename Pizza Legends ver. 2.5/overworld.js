201bf0a0ae49801570117d3deb3bdc622cd65176
Juan Manuel Pachas <juanmapv387@gmail.com>
2024-08-04 19:04:18 -0500

    Add files via upload

---
+++ Pizza Legends ver. 2.5/overworld.js
@@ -0,0 +1,52 @@
+class Overworld {
+
+constructor(config) {
+this.element = config.element;
+this.canvas = this.element.querySelector(".game-canvas");
+this.ctx = this.canvas.getContext("2d");
+this.map = null;
+}
+
+startGameLoop(){
+    const step = () => {
+    
+    this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+
+    //Establish camera person
+    const cameraPerson = this.map.gameObjects.hero;
+
+    //Update all objects
+    Object.values(this.map.gameObjects).forEach(object =>{
+        object.update({
+            arrow: this.directionInput.direction
+            });
+    })
+
+    //Draw Lower Layer
+    this.map.drawLowerImage(this.ctx, cameraPerson)
+
+    //Draw Game Objects
+    Object.values(this.map.gameObjects).forEach(object =>{
+        
+        object.sprite.draw(this.ctx, cameraPerson);
+    })
+
+    //Draw Upper Layer
+    this.map.drawUpperImage(this.ctx, cameraPerson)
+
+        // console.log("stepping!");
+    requestAnimationFrame(()=> {
+        step();
+    })
+    }
+    step();
+}
+
+init() {
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
+    this.directionInput.direction; 
+     this.startGameLoop();
+}
+}
